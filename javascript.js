ca8762a5df9244ab08b933d518c63b374f3b5e42
rikkeblom <rikke_blom@hotmail.com>
2020-11-03 10:59:25 +0100

    Trump is now clickable - stops and then restarts the animation

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
-document.querySelector("#trump1-sprite").classList.add("upDown");
-document.querySelector("#trump1-sprite").addEventListener("click", stop);
+window.addEventListener("load", start);
+
+function start() {
+    console.log("function start()")
+    document.querySelector("#trump1-sprite").classList.add("upDown");
+    document.querySelector("#trump1-container").classList.add("pos1");
+    document.querySelector("#trump1-sprite").addEventListener("click", stop);
+}
 
 function stop() {
     console.log("function stop()");
     document.querySelector("#trump1-sprite").classList.add("stop");
+    restart();
+}
+
+function restart() {
+    console.log("function restart()")
+    document.querySelector("#trump1-sprite").classList.remove("stop")
+    document.querySelector("#trump1-sprite").classList.remove("upDown");
+    document.querySelector("#trump1-container").classList.remove("pos1");
+    document.querySelector("#trump1-container").offsetHeight;
+    start();
 }
 
